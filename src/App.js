c90c8a1c7d86e73299209fa414d421b483cd9f07
Aneena John Xavier <aneena_john_xavier@epam.com>
2023-10-06 14:30:06 +0530

    Setup for tailwind css is done
    Signed-off-by: Aneena John Xavier <aneena_john_xavier@epam.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./styles.css";
+import "./main.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{jsx,js}", "./src/components/*.{jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
