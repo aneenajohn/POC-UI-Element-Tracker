470b620fd7ced55dcffb5015662ebf2ab2ccf283
Nishant Arya1 <nishant_arya1@epam.com>
2023-10-13 11:02:14 +0530

    test commit and push

--- src/pages/Elements.jsx
+++ src/pages/Elements.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Elements = () => {
   return (
     <div className="flex-grow text-center font-medium py-6">
-      <p>Will Render the created UI elements here</p>
-      <p>The user can edit/ delete from this page</p>
+      <p>Will Render the created UI elements here.</p>
+      <p>The user can edit/ delete from this page.</p>
     </div>
   )
 }
