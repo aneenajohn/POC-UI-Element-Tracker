8a4fdc23df10988745f5b4a4a82ca9ac764063aa
Aneena John Xavier <aneena_john_xavier@epam.com>
2023-10-31 18:49:47 +0530

    Integrated create ui element post api
    Signed-off-by: Aneena John Xavier <aneena_john_xavier@epam.com>

--- src/pages/CreateElements.jsx
+++ src/pages/CreateElements.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { InputWithLabel, ButtonDetails, InputDetails } from '../components';
 
-import { BUTTON_INITIAL_VALUES, INPUT_INITIAL_VALUES } from '../constants';
+import { BUTTON_INITIAL_VALUES, INPUT_INITIAL_VALUES, BASE_URL, API } from '../constants';
 
 export const CreateElements = () => {
 
@@ -15,6 +15,8 @@ export const CreateElements = () => {
   const [inputAttributes, setInputAttributes] = useState(INPUT_INITIAL_VALUES)
 
   const [isButtonDisabled, setButtonDisabled] = useState(true);
+
+  const [alertDetails, setAlertDetails] = useState({});
 
   const resetUiAttributes = () => {
     setButtonAttributes(BUTTON_INITIAL_VALUES)
@@ -45,6 +47,74 @@ export const CreateElements = () => {
   useEffect(() => {
     console.log("Inputs: ", inputs);
   },[inputs]);
+
+  const getLocatorElements = (uiElementType) => {
+    switch(uiElementType) {
+        case "button":
+            return {
+                name: buttonAttributes.name,
+                tag: buttonAttributes.tag,
+                type: buttonAttributes.type,
+                value: buttonAttributes.value,
+            }
+        case "input":
+            return {
+                name: inputAttributes.name,
+                tag: inputAttributes.tag,
+                type: inputAttributes.type,
+                value: inputAttributes.value,
+            }
+        default:
+            return
+    }
+  }
+
+  const getStyles = (uiElementType) => {
+    switch(uiElementType) {
+        case "button":
+            return buttonAttributes.styles
+        case "input":
+            return inputAttributes.styles
+        default:
+            return
+    }
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const formData =  {
+        data: [
+            {
+                pageName: inputs.pageName,
+                elements: [
+                    {
+                        locator: getLocatorElements(inputs.uiElementType),
+                        styles: getStyles(inputs.uiElementType)
+                    }
+                ]
+            }
+        ]
+    }
+
+    try {
+        const response = await API.post('/', formData);
+        console.log("Resp: ", response.data);
+        if(response.status === "SUCCESS") {
+            setAlertDetails({
+                title: "Success!",
+                message: "UI Element created successfully"
+            })
+        }
+    }catch (error) {
+        console.error('Error posting data:', error);
+        // throw error;
+        setAlertDetails({
+            title: "Failure!",
+            message: "UI Element creation failed"
+        })
+    }
+  }
 
   useEffect(() => {
     console.log("Btn Atrr from Create main", buttonAttributes)
@@ -87,8 +157,10 @@ export const CreateElements = () => {
                     setInputAttributes={setInputAttributes}
                 />) : null}
             <button
-                className={`px-6 py-3 bg-primary text-text_secondary mb-4 ${isButtonDisabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer opacity-1'}`}
-                disabled={isButtonDisabled}
+                // className={`px-6 py-3 bg-primary text-text_secondary mb-4 ${isButtonDisabled ? 'cursor-not-allowed opacity-50' : 'cursor-pointer opacity-1'}`}
+                // disabled={isButtonDisabled}
+                className={`px-6 py-3 bg-primary text-text_secondary mb-4`}
+                onClick={submitHandler}
             >
                 Create
             </button>
