a47d0b054969fe7cab3be588fd1f3e06f933af7f
Nishant Arya1 <nishant_arya1@epam.com>
2023-11-06 12:44:10 +0530

    data fetched from api and delete functionality completed

--- src/pages/Elements.jsx
+++ src/pages/Elements.jsx
@@ -1,41 +1,84 @@
 import React, { useEffect, useState } from "react";
-import Modal from '../components/Modal'
+import Modal from "../components/Modal";
+import Loader from '../components/Loader'
 import { demoData } from "../MockData";
+import {
+  BUTTON_INITIAL_VALUES,
+  INPUT_INITIAL_VALUES,
+  BASE_URL,
+  API,
+} from "../constants";
+import Alert from "../components/Alert";
 
 export const Elements = () => {
-  const [showModal,handleShowModal]=useState(false);
-  const [loader,handleLoader]=useState(false);
-  const [data,setData]=useState([]);
-  const [elementId,setElementId]=useState('')
+  const [showModal, handleShowModal] = useState(false);
+  const [loader, handleLoader] = useState(false);
+  const [alert, handleAlert] = useState(false);
+  const [data, setData] = useState([]);
+  const [elementId, setElementId] = useState("");
 
-
-  useEffect(()=>{
-    setData(demoData);
-  },[]);
+  const getData = async () => {
+    try {
+      handleLoader(true);
+      const response = await API.get("/");
+      console.log("Resp: ", response.data);
+      setData(response.data);
+      handleLoader(false)
+      setTimeout(() => {
+        handleAlert(false);
+      }, 3000);
+    } catch (error) {
+      console.error("Error posting data:", error);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   const handleEdit = () => {
     console.log("handle edit");
-  }
+  };
 
-  const handleClickDelete = (e,pageName) => {
+  const handleClickDelete = (e, id) => {
     handleShowModal(true);
-    setElementId(pageName)
-    console.log("handle edit",pageName);
-  }
-  const handleDelete = (e) => {
-    handleShowModal(false)
+    setElementId(id);
+    console.log("handle edit", id);
+  };
+  const handleDelete = async (e) => {
+    handleShowModal(false);
     handleLoader(true);
-  }
-  
+    console.log(elementId);
+    const response = await API.delete(`/${elementId}`);
+    if (response.data.status === "SUCCESS") {
+      handleAlert(true);
+    }
+    
+    getData();
+  };
+
   return (
     <>
-    {showModal?<Modal show={loader} handleShowModal={handleShowModal} handleDelete={handleDelete} />:''}
+   
+    {loader ? <Loader/> : ""}
+      {alert ? <Alert /> : ""}
+      {showModal ? (
+        <Modal
+          show={loader}
+          handleShowModal={handleShowModal}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        ""
+      )}
       <h5 className="mb-2 text-3xl text-center font-bold tracking-tight text-gray-900 dark:text-white">
         Pages and Elements
       </h5>
-      <div className="flex gap-2 justify-center text-center font-medium py-6">
+      <div className="flex gap-2 w-full flex-wrap justify-center text-center font-medium py-6">
         {data?.data?.map((value) => {
           return (
-            <div className="block w-52 p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700" key={value.pageName}>
+            <div
+              className="block w-52 p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+              key={value._id}
+            >
               <div className="flex flex-row justify-between">
                 <span className="cursor-pointer" onClick={handleEdit}>
                   <svg
@@ -53,7 +96,10 @@ export const Elements = () => {
                     />
                   </svg>
                 </span>
-                <span className="cursor-pointer" onClick={(e)=>handleClickDelete(e,value.pageName)}>
+                <span
+                  className="cursor-pointer"
+                  onClick={(e) => handleClickDelete(e, value._id)}
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -63,7 +109,7 @@ export const Elements = () => {
                     className="w-6 h-6"
                   >
                     <path
-                     strokeLinecap="round"
+                      strokeLinecap="round"
                       strokeLinejoin="round"
                       d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
                     />
@@ -72,16 +118,20 @@ export const Elements = () => {
               </div>
 
               <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {value.pageName}
+                {Array.isArray(value.data)
+                  ? value.data[0].pageName
+                  : "No page "}
               </h5>
               <div className="font-normal text-gray-700 dark:text-gray-400">
                 <h6 className="mb-2  font-bold tracking-tight text-gray-900 dark:text-white">
                   Elements
                 </h6>
                 <ul className="my-4 ">
-                  {value.elements.map((ele) => {
-                    return <li key={ele.locator.value}>{ele.locator.name}</li>;
-                  })}
+                  {Array.isArray(value.data)
+                    ? value.data.map((ele) => {
+                        return <li>{ele?.elements[0]?.locator?.name || ""}</li>;
+                      })
+                    : null}
                 </ul>
               </div>
             </div>
